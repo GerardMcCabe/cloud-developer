87d04c03d278a13dcfc87bec14040227c02128fa
Gerard <gerardmccabe@gmail.com>
2020-11-26 22:39:38 +0000

    Config.ts settings

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "udagramdatabase2",
+    "password": "Embedded2910",
+    "database": "udagramdatabase2",
+    "host": "udagramdatabase2.cpsfprzypq8g.eu-west-1.rds.amazonaws.com",
     "dialect": "postgres",
-    "aws_region": "us-east-2",
+    "aws_region": "eu-west-1a",
     "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_media_bucket": "udagram-mccabe-dev-bucket"
   },
   "prod": {
     "username": "",
